34e6b74db8992eaf316de82ce5d8e4d4c8f5ee1f
zyp <2550302293@qq.com>
2019-08-29 14:26:46 +0800

    上传koltin pro

--- App/src/main/java/com/kotlin/mall/ui/fragment/HomeFragment.kt
+++ App/src/main/java/com/kotlin/mall/ui/fragment/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kotlin.base.common.BaseApplication.Companion.context
 import com.kotlin.base.ext.onClick
 import com.kotlin.base.ui.fragment.BaseFragment
 import com.kotlin.base.widgets.BannerImageLoader
@@ -20,16 +19,15 @@ import kotlinx.android.synthetic.main.fragment_home.*
 import me.crosswall.lib.coverflow.CoverFlow
 import org.jetbrains.anko.support.v4.startActivity
 import org.jetbrains.anko.support.v4.toast
-import javax.xml.transform.Transformer
 
 /*
     主界面Fragment
  */
-class HomeFragment:BaseFragment() {
+class HomeFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreateView(inflater, container,savedInstanceState)
-        return inflater.inflate(R.layout.fragment_home,null)
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_home, null)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
@@ -72,7 +70,7 @@ class HomeFragment:BaseFragment() {
     /*
         初始化公告
      */
-    private fun initNews(){
+    private fun initNews() {
         //公告
         mNewsFlipperView.setData(arrayOf("夏日炎炎，第一波福利还有30秒到达战场", "新用户立领1000元优惠券"))
     }
@@ -80,12 +78,12 @@ class HomeFragment:BaseFragment() {
     /*
         初始化折扣
      */
-    private fun initDiscount(){
+    private fun initDiscount() {
         val manager = LinearLayoutManager(context)
         manager.orientation = LinearLayoutManager.HORIZONTAL
         mHomeDiscountRv.layoutManager = manager
 
-        val discountAdapter  = HomeDiscountAdapter(activity)
+        val discountAdapter = HomeDiscountAdapter(activity)
         mHomeDiscountRv.adapter = discountAdapter
         discountAdapter.setData(mutableListOf(HOME_DISCOUNT_ONE, HOME_DISCOUNT_TWO, HOME_DISCOUNT_THREE, HOME_DISCOUNT_FOUR, HOME_DISCOUNT_FIVE))
     }
@@ -93,7 +91,7 @@ class HomeFragment:BaseFragment() {
     /*
         初始化主题
      */
-    private fun initTopic(){
+    private fun initTopic() {
         //话题
         mTopicPager.adapter = TopicAdapter(context, listOf(HOME_TOPIC_ONE, HOME_TOPIC_TWO, HOME_TOPIC_THREE, HOME_TOPIC_FOUR, HOME_TOPIC_FIVE))
         mTopicPager.currentItem = 1
